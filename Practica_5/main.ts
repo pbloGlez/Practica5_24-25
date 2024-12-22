f7fef4c482ac819ec359c9f1047936a500206ca9
PabloGlez <113451860+pbloGlez@users.noreply.github.com>
2024-12-22 23:07:07 +0100

    Update main.ts

--- Practica_5/main.ts
+++ Practica_5/main.ts
@@ -1,33 +1,32 @@
-import { ApolloServer } from "@apollo/server";
-import { schema } from "./schema.ts";
 import { MongoClient } from "mongodb";
-import { CommentModel, PostModel, UserModel } from "./types.ts";
+import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { resolvers } from "./resolvers.ts";
+import { schema } from "./schema.ts";
+import { CommentModel, PostModel, UserModel } from "./types.ts";
 
 const MONGO_URL = Deno.env.get("MONGO_URL");
 
 if (!MONGO_URL) {
-  throw new Error("Please provide a MONGO_URL");
+  throw new Error("No se ha encontrado la varaible de entorno MONGO_URL");
 }
 
 const mongoClient = new MongoClient(MONGO_URL);
 await mongoClient.connect();
 
-console.info("Connected to MongoDB");
+console.info("Conectado a MongoDB");
 
-const mongoDB = mongoClient.db("Practica5");
-const UserCollection = mongoDB.collection<UserModel>("users");
-const CommentCollection = mongoDB.collection<CommentModel>("comments");
-const PostCollection = mongoDB.collection<PostModel>("posts");
+const mongoDB = mongoClient.db("practica5");
+
+const UsersCollection = mongoDB.collection<UserModel>("users");
+const PostsCollection = mongoDB.collection<PostModel>("posts");
+const CommentsCollection = mongoDB.collection<CommentModel>("comments");
 
 const server = new ApolloServer({
   typeDefs: schema,
   resolvers,
 });
 
-const { url } = await startStandaloneServer(server, {
-  context: async () => ({ UserCollection, CommentCollection, PostCollection }),
-});
+const { url } = await startStandaloneServer(server, { context: async () => ({ UsersCollection, PostsCollection, CommentsCollection })});
 
 console.info(`Server ready at ${url}`);
